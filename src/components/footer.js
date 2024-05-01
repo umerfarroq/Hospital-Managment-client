e4304804e880b5d4a4cfd4f6e9624e775b965e75
Umar Farooq <125653666+umerfarroq@users.noreply.github.com>
2024-05-01 18:48:41 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,29 +1,69 @@
-import React from 'react';
-import './styles/Footer.css'; // Import your CSS file for footer styles
+import React from "react";
+import "./styles/Footer.css"; // Import your CSS file for footer styles
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        <div className="footer-section">
-          <h4>About Us</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
+        <div className="adress-contact-container">
+          <div className="footer-hospitalAdress">
+            <h4>Acme Hospital</h4>
+            <p>123 Main st</p>
+            <p>Anytwon,USA 1234</p>
+          </div>
+          <div className="footer-section" id="footer-contact">
+            <h4>Contact Information</h4>
+            <p>123 Hospital Street, City</p>
+            <p>Phone: +1 234 567 890</p>
+            <p>Phone: +1 221 509 811</p>
+            <p>Email: info@hospital.com</p>
+          </div>
         </div>
-        <div className="footer-section">
-          <h4>Contact Information</h4>
-          <p>123 Hospital Street, City</p>
-          <p>Phone: +1 234 567 890</p>
-          <p>Email: info@hospital.com</p>
-        </div>
-        <div className="footer-section">
-          <h4>Quick Links</h4>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/departments">Departments</a></li>
-            <li><a href="/doctors">Doctors</a></li>
-            <li><a href="/blog">Blog</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
+
+        <div className="aboutUs-socailMedia-container">
+          <div className="footer-section" id="footer-sec-about-us">
+            <h4>Follow us</h4>
+            <ul className="footer-ul">
+              <li>
+                <i class="ri-instagram-line" id="socailMedialIcons"></i>
+              </li>
+              <li>
+                <i class="ri-facebook-circle-fill" id="socailMedialIcons"></i>
+              </li>
+              <li>
+                <i class="ri-twitter-x-line" id="socailMedialIcons"></i>
+              </li>
+            </ul>
+           
+          </div>
+
+          <div className="footer-section" id="footer-sec-links">
+            <h4>Quick Links</h4>
+
+            <ul>
+              <li>
+                <a href="/">Home</a>
+                {/* <Link to={"/"}>Home</Link> */}
+              </li>
+              <li>
+                <a href="/departments">Departments</a>
+                {/* <Link to="/departments">Departments</Link> */}
+              </li>
+              <li>
+                <a href="/doctors">Doctors</a>
+                {/* <Link to="/doctors">Doctors</Link> */}
+              </li>
+              <li>
+                <a href="/blog">Blog</a>
+                {/* <Link to="/blog">Blog</Link> */}
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+                {/* <Link to="/contact">Contact</Link> */}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className="footer-bottom">
